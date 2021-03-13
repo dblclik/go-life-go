9650bd249e994559b969e9fd7b6a67fc7693bc68
Ben Elbert <ben@Bens-MacBook-Pro.local>
2021-03-12 22:06:44 -0500

    working version

--- evolution.go
+++ evolution.go
@@ -1,6 +1,4 @@
-package golife
-
-import "sync"
+package main
 
 /*
 These rules, which compare the behavior of the automaton to real life,
@@ -11,60 +9,61 @@ can be condensed into the following:
 	- All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 */
 
-func evolve(x int, y int, matIn [][]int, matOut [][]int, wg *sync.WaitGroup) {
-	// Start with an active count of 0
-	defer wg.Done()
+// Evolve iterates the matrix forward by 1 step
+// func Evolve(x int, y int, matIn [][]int, matOut [][]int, wg *sync.WaitGroup) {
+// 	// Start with an active count of 0
+// 	defer wg.Done()
 
-	activeCells := 0
-	yMax := len(matIn)
-	xMax := len(matIn[0])
+// 	activeCells := 0
+// 	yMax := len(matIn)
+// 	xMax := len(matIn[0])
 
-	// Need to iterate over 3x3 region centered at (x,y) and sum
+// 	// Need to iterate over 3x3 region centered at (x,y) and sum
 
-	if y-1 >= 0 {
-		if x-1 >= 0 {
-			activeCells += matIn[y-1][x-1]
-		}
+// 	if y-1 >= 0 {
+// 		if x-1 >= 0 {
+// 			activeCells += matIn[y-1][x-1]
+// 		}
 
-		// UC for free (No need to check x bound here)
-		activeCells += matIn[y-1][x]
+// 		// UC for free (No need to check x bound here)
+// 		activeCells += matIn[y-1][x]
 
-		if x+1 < xMax {
-			activeCells += matIn[y-1][x+1]
-		}
-	}
+// 		if x+1 < xMax {
+// 			activeCells += matIn[y-1][x+1]
+// 		}
+// 	}
 
-	if x-1 >= 0 {
-		activeCells += matIn[y][x-1]
-	}
+// 	if x-1 >= 0 {
+// 		activeCells += matIn[y][x-1]
+// 	}
 
-	if x+1 < xMax {
-		activeCells += matIn[y][x+1]
-	}
+// 	if x+1 < xMax {
+// 		activeCells += matIn[y][x+1]
+// 	}
 
-	if y+1 < yMax {
-		if x-1 >= 0 {
-			activeCells += matIn[y+1][x-1]
-		}
+// 	if y+1 < yMax {
+// 		if x-1 >= 0 {
+// 			activeCells += matIn[y+1][x-1]
+// 		}
 
-		// LC for free (No need to check x bound here)
-		activeCells += matIn[y+1][x]
+// 		// LC for free (No need to check x bound here)
+// 		activeCells += matIn[y+1][x]
 
-		if x+1 < xMax {
-			activeCells += matIn[y+1][x+1]
-		}
-	}
+// 		if x+1 < xMax {
+// 			activeCells += matIn[y+1][x+1]
+// 		}
+// 	}
 
-	if activeCells > 3 || activeCells < 2 {
-		matOut[y][x] = 0
-	} else {
-		if activeCells == 3 {
-			matOut[y][x] = 1
-		} else {
-			matOut[y][x] = matIn[y][x]
-		}
-	}
-}
+// 	if activeCells > 3 || activeCells < 2 {
+// 		matOut[y][x] = 0
+// 	} else {
+// 		if activeCells == 3 {
+// 			matOut[y][x] = 1
+// 		} else {
+// 			matOut[y][x] = matIn[y][x]
+// 		}
+// 	}
+// }
 
 // // MinMax is a one-pass min and max finder for slices
 // func MinMax(arr []int) (int, int) {
--- main.go
+++ main.go
@@ -1,29 +1,42 @@
-package golife
+package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
 	"sync"
+	"time"
 )
 
 func main() {
-	const iterations int = 3
+	const iterations int = 50
 
-	currentState := [][]int{
-		{0, 0, 1, 1, 0},
-		{0, 0, 1, 1, 0},
-		{0, 0, 0, 0, 0},
-		{0, 0, 0, 1, 1},
-		{0, 1, 0, 0, 1},
+	currentState := [][]int{}
+	height := 10 + rand.Intn(30)
+	width := 10 + rand.Intn(50)
+
+	for i := 0; i < height; i++ {
+		row := []int{}
+		for j := 0; j < width; j++ {
+			if rand.Float32() < 0.5 {
+				row = append(row, 0)
+			} else {
+				row = append(row, 1)
+			}
+		}
+		currentState = append(currentState, row)
 	}
 
-	fmt.Println(currentState)
+	PrintMatrix(currentState)
+	fmt.Println()
+	time.Sleep(500 * time.Millisecond)
+	CallClear()
 
 	var wg sync.WaitGroup
 
-	height := len(currentState)
-	width := len(currentState[0])
-
-	for i := 0; i < iterations; i++ {
+	for iter := 0; iter < iterations; iter++ {
 		duplicate := make([][]int, len(currentState))
 		for i := range currentState {
 			duplicate[i] = make([]int, len(currentState[i]))
@@ -31,14 +44,106 @@ func main() {
 		}
 		for j := 0; j < height; j++ {
 			for k := 0; k < width; k++ {
-				go evolve(k, j, currentState, duplicate, &wg)
+				wg.Add(1)
+				go Evolve(k, j, currentState, duplicate, &wg)
 			}
 		}
-		wg.Wait()
 
-		currentState := duplicate
+		currentState = duplicate
 		duplicate = nil
+		PrintMatrix(currentState)
+		fmt.Println()
+		time.Sleep(50 * time.Millisecond)
+		CallClear()
+	}
+	wg.Wait()
+}
+
+var clear map[string]func() //create a map for storing clear funcs
+
+func init() {
+	clear = make(map[string]func()) //Initialize it
+	clear["darwin"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+// CallClear lets us clear the terminal for easier printing of the matrices
+func CallClear() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+}
+
+// PrintMatrix pretty prints the given matrix
+func PrintMatrix(matrix [][]int) {
+	for _, row := range matrix {
+		fmt.Println(row)
+	}
+}
+
+// Evolve iterates the matrix forward by 1 step
+func Evolve(x int, y int, matIn [][]int, matOut [][]int, wg *sync.WaitGroup) {
+	// Start with an active count of 0
+	defer wg.Done()
+
+	activeCells := 0
+	yMax := len(matIn)
+	xMax := len(matIn[0])
+
+	// Need to iterate over 3x3 region centered at (x,y) and sum
+
+	if y-1 >= 0 {
+		if x-1 >= 0 {
+			activeCells += matIn[y-1][x-1]
+		}
+
+		// UC for free (No need to check x bound here)
+		activeCells += matIn[y-1][x]
+
+		if x+1 < xMax {
+			activeCells += matIn[y-1][x+1]
+		}
 	}
 
-	fmt.Println(currentState)
+	if x-1 >= 0 {
+		activeCells += matIn[y][x-1]
+	}
+
+	if x+1 < xMax {
+		activeCells += matIn[y][x+1]
+	}
+
+	if y+1 < yMax {
+		if x-1 >= 0 {
+			activeCells += matIn[y+1][x-1]
+		}
+
+		// LC for free (No need to check x bound here)
+		activeCells += matIn[y+1][x]
+
+		if x+1 < xMax {
+			activeCells += matIn[y+1][x+1]
+		}
+	}
+
+	if activeCells > 3 || activeCells < 2 {
+		matOut[y][x] = 0
+	} else {
+		if activeCells == 3 {
+			matOut[y][x] = 1
+		} else {
+			matOut[y][x] = 1 * matIn[y][x]
+		}
+	}
 }
